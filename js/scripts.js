3fefdc4f918c91b77db43f085eced8660cf02647
Dylan Lewis and Larry Taylor <larry@>
2017-06-19 13:12:07 -0700

    complete landing page

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+  $("#fransatan").click(function(){
+    $("#satan").toggle();
+    $("#francis").toggle();
+  })
+
+  $("#start").submit(function(event) {
+    event.preventDefault()
+    var name = $("#name").val();
+    var description = $("#goodDeed").val();
+    var goodness = $("#howGood").val();
+    var greeting = "Welcome, " + name + "!" + " it's time to find out if you're a good person.";
+    $("#nameResponse").prepend(greeting)
+    $("#start").fadeOut();
+    $("#quiz-start").fadeIn();
+
+  });
+});
