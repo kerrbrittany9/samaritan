b0c94b14cba6284690d1684cd9944e791c06be3a
Brittany Kerr, Calla Rudolph, Dylan Lewis and Larry Taylor <brittany@>
2017-06-19 16:23:15 -0700

    modify css of pg 3 and pg 4

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ $(document).ready(function(){
     $("input:checkbox[name=acts]:checked").each(function(){
       var acts = $(this).val();
     });
+$(document).ready(function(){
+  $("form#gifts").submit(function(event){
+    event.preventDefault();
+    $("input:checkbox[name=gifts]:checked").each(function(){
+      var gifts = $(this).val();
+    });
+    var inputtedCans = $("input#can").val();
+    var inputtedGoodwill = $("input#goodwill").val();
